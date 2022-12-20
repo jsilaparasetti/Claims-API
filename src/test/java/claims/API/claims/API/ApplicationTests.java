b967a1e66d7035697b0cfde9b3b321b95091315f
jsilaparasetti <jsilaparasetti@miraclesoft.com>
2022-12-20 20:02:34 +0530

    new updates

---
+++ src/test/java/claims/API/claims/API/ApplicationTests.java
@@ -0,0 +1,13 @@
+package claims.API.claims.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
